5c451918682e2754ea443232417920af29dd7abd
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-21 01:28:15 +0530

    navigates to different routes programatically after an action

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate(); //returns a function
+  //now using this navigate function, we can navigate to the order-summary route on clik of a button, just provide --> as argument ----> the path for the route or a number that indicates a change in the history stack
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>Home page</div>
+      <button onClick={() => navigate("order-summary")}>Place order</button>
+    </>
+  );
+};
 
-export default Home
+export default Home;
