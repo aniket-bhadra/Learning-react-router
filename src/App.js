ce7ac997d4d38f34346606ed7f08adb0edc2f7ff
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-02-20 23:38:22 +0530

    Navbar added for easy navigatigation to the urls

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 //for route configuration need 2 components from react router
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
