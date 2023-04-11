a829b1434b5e2bf81713921266ed70ed173893dd
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-04-11 12:54:50 +0530

    route params implmented

--- src/components/user/UserDetails.js
+++ src/components/user/UserDetails.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const UserDetails = () => {
-  return (
-    <div>UserDetails</div>
-  )
-}
+  const params = useParams(); //return an object of dynamic key value pair from the current url
 
-export default UserDetails
+  const userID = params.userID;
+  return <div>Details about the user {userID}</div>;
+};
+
+export default UserDetails;
+
+//this same component has to be rendered for three different urls
