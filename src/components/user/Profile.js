be7b4f3e27963d2e815c99904e5c1ff480f26d3f
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-04-11 13:22:53 +0530

    displayed the loggedin username in profile page & also add a logout functionality

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../store/auth";
 
 const Profile = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+  
+  const logoutHandler = () => {
+    auth.logout();
+    navigate("/");
+  };
   return (
-    <div>Profile</div>
-  )
-}
+    <div>
+      Welcome {auth.user}
+      <button onClick={logoutHandler}>Logout</button>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
 
 //this component is belongs to protected route. & accessible only if the user is logged in
